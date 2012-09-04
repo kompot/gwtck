9543f1302c0485faa36250cd6a349b6831b4691b
Anton Fedchenko <kompotik@gmail.com>
2012-09-04 20:13:13 +0400

    CKEditor version downgraded to 3.6.2 as 3.6.3 because of a weird bug.
    Also some compatibility fixes for building project on OS X.
    Hosting page began to scroll when CKEditor window was more than 4 lines. Appeared only in Safari/Chrome.
    Downgrading solved this problem.
    Version 0.0.2 -> 0.0.3.

--- update-ckeditor-to-latest-trunk.sh
+++ update-ckeditor-to-latest-trunk.sh
@@ -15,12 +15,12 @@
 # http://docs.cksource.com/CKEditor_3.x/Developers_Guide/CKPackager
 # for more info.
 
-JAVA_HOME="/usr/lib/jvm/java-6-sun"
+#JAVA_HOME="/usr/lib/jvm/java-6-sun"
 CKEDITOR_TRUNK="ckeditor-trunk"
 CKEDITOR_RELEASE="ckeditor-release"
 CKEDITOR_MODIFIED="ckeditor-modified"
 GWT_CKEDITOR_PUBLIC="src/main/resources/com/gmail/kompotik/gwtck/public/ckeditor/"
-CKEDITOR_VERSION="3.6.3"
+CKEDITOR_VERSION="3.6.2"
 
 rm -rf $CKEDITOR_TRUNK
 rm -rf $CKEDITOR_RELEASE
@@ -28,14 +28,14 @@ rm -rf $GWT_CKEDITOR_PUBLIC
 mkdir -p $GWT_CKEDITOR_PUBLIC
 svn export http://svn.fckeditor.net/CKEditor/tags/$CKEDITOR_VERSION/ $CKEDITOR_TRUNK
 # work in these 2 lines should be done by `diff` but it behaves weird (?) and compares files in a strange way
-mkdir --parent $CKEDITOR_TRUNK/_source/plugins/imagepaste/
+mkdir -p $CKEDITOR_TRUNK/_source/plugins/imagepaste/
 cp $CKEDITOR_MODIFIED/_source/plugins/imagepaste/plugin.js $CKEDITOR_TRUNK/_source/plugins/imagepaste/
 
 diff -cr $CKEDITOR_TRUNK $CKEDITOR_MODIFIED > modifications.patch
 patch -p0 -i modifications.patch
 rm modifications.patch
 #$JAVA_HOME/bin/java -jar $CKEDITOR_TRUNK/_dev/packager/ckpackager/ckpackager.jar $CKEDITOR_TRUNK/ckeditor.pack
-$JAVA_HOME/bin/java -jar $CKEDITOR_TRUNK/_dev/releaser/ckreleaser/ckreleaser.jar $CKEDITOR_TRUNK/_dev/releaser/ckreleaser.release $CKEDITOR_TRUNK/ ./$CKEDITOR_RELEASE/ "trunk" ckeditor_trunk --run-before-release=$LANGTOOL
+java -jar $CKEDITOR_TRUNK/_dev/releaser/ckreleaser/ckreleaser.jar $CKEDITOR_TRUNK/_dev/releaser/ckreleaser.release $CKEDITOR_TRUNK/ ./$CKEDITOR_RELEASE/ "trunk" ckeditor_trunk --run-before-release=$LANGTOOL
 #cp $CKEDITOR_RELEASE/release/ckeditor_source.js $CKEDITOR_RELEASE/release/ckeditor.js $CKEDITOR_RELEASE/release/ckeditor_basic.js $CKEDITOR_RELEASE/release/config.js $GWT_CKEDITOR_PUBLIC/
 cp $CKEDITOR_RELEASE/release/ckeditor.js $CKEDITOR_RELEASE/release/ckeditor_basic.js $CKEDITOR_RELEASE/release/config.js  $GWT_CKEDITOR_PUBLIC/
 #cp -r $CKEDITOR_RELEASE/release/source $CKEDITOR_RELEASE/release/plugins  $CKEDITOR_RELEASE/release/skins $CKEDITOR_RELEASE/release/lang $GWT_CKEDITOR_PUBLIC/
